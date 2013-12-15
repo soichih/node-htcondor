565019645153c00c90a34a2006ac3d74562d7548
Soichi Hayashi <hayashis@iu.edu>
2013-12-15 19:13:26 +0000

    Added a very basic README.md

--- README.md
+++ README.md
@@ -2,3 +2,49 @@ node-htcondor
 =============
 
 nodejs hook for htcondor
+
+To install:
+
+```bash
+npm install htcondor
+```
+
+#Use:
+```javascript
+var eventlog = require('htcondor').eventlog
+
+//you can start listening on your htcondor eventlog
+eventlog.listen("/var/log/condor/EventLog");
+
+//and receive events
+eventlog.on(function(event) {
+    console.dir(event);
+});
+````
+
+Currently, on() will capture all classad update event (eventid 28). You will receive an object that looks like
+
+```
+{ _eventid: 28,
+  _jobid: '49563264.000.000',
+  _timestamp: '12/15 19:12:25',
+  _updatetime: Sun Dec 15 2013 19:12:25 GMT+0000 (UTC),
+  Proc: 0,
+  EventTime: '2013-12-15T19:12:25',
+  TriggerEventTypeName: 'ULOG_SUBMIT',
+  SubmitHost: '<129.79.53.21:9615?sock=8287_a430_1068600>',
+  QDate: 1387134745,
+  TriggerEventTypeNumber: 0,
+  MyType: 'SubmitEvent',
+  Owner: 'donkri',
+  CurrentHosts: 0,
+  GlobalJobId: 'osg-xsede.grid.iu.edu#49563264.0#1387134745',
+  Cluster: 49563264,
+  AccountingGroup: 'group_xsedelow.donkri',
+  Subproc: 0,
+  EventTypeNumber: 28,
+  CurrentTime: 'time()' }
+```
+
+#License
+MIT. Please see License file for more details.
---
+++ eventlog.js
@@ -0,0 +1,252 @@
+var fs = require('fs');
+var xml2js = require('xml2js');
+
+var callbacks = [];
+
+exports.eventlog = {
+    on: function(callback) {
+        callbacks.push(callback);
+    },
+    listen: function(path) {
+        console.log("eventlog lister listening on "+path);
+        //tail eventlog
+        var stats = fs.statSync(path);
+        var buffer = new Buffer(64);
+        var block = "";
+        var delim = "...\n";
+        fs.watch(path, function(event) {
+            if(event == "rename") {
+                stats.size = 0; //TODO - I am not sure if this is what we need to do?
+            } else if(event == "change") {
+                //make sure the target file still exists..
+                fs.exists(path, function(exists) {
+                    var new_stats = fs.statSync(path);
+                    //let's read the new content
+                    fs.open(path, "r", function(err, fd) {
+                        while(stats.size < new_stats.size) {
+                            //read some in sync..
+                            var size = fs.readSync(fd, buffer, 0, buffer.length, stats.size);
+                            stats.size += size;
+                            block = block + buffer.toString("ascii", 0, size);
+
+                            //search for delimiter
+                            var pos = block.indexOf(delim);
+                            if(pos != -1) {
+                                ablock = block.substring(0, pos);
+                                block = block.substring(pos+delim.length);
+                                var lines = ablock.split("\n");
+                                var header = lines[0];
+
+                                //parse header
+                                var props = {
+                                    _eventid: parseInt(header.substring(0,3)),
+                                    _jobid: header.substring(5,21),
+                                    _timestamp: header.substring(23,37),
+                                    _updatetime: new Date()
+                                };
+
+                                //only listen to class ad update event (for now..)
+                                if(props._eventid == 28) {
+                                    //parse event body
+                                    var body = lines.slice(1);
+                                    body.forEach(function(line) {
+                                        if(line == "") return;
+                                        var kv = line.split(" = ");
+                                        //remove double quote from value if it's quoted
+                                        if(kv[1].indexOf("\"") == 0) {
+                                            kv[1] = kv[1].substring(1, kv[1].length-1);
+                                        }
+                                        //convert to int if its int
+                                        var i = parseInt(kv[1]);
+                                        if(i == kv[1]) {
+                                            kv[1] = i;
+                                        }
+                                        props[kv[0]] = kv[1];
+                                    });
+                                    //jobs[clusterid](props);
+                                    callbacks.forEach(function(callback) {
+                                        callback(props);
+                                    });
+                                }
+                                /*
+                                //do we care?
+                                var jobid_tokens = props._jobid.split(".");
+                                var clusterid = jobid_tokens[0];
+                                var procid = jobid_tokens[1];
+                                if(jobs[clusterid] != undefined) {
+                                    if(props.eventid == 28) {//class ad update event
+                                        //parse event body
+                                        var body = lines.slice(1);
+                                        body.forEach(function(line) {
+                                            if(line == "") return;
+                                            var kv = line.split(" = ");
+                                            //remove double quote from value if it's quoted
+                                            if(kv[1].indexOf("\"") == 0) {
+                                                kv[1] = kv[1].substring(1, kv[1].length-1);
+                                            }
+                                            //convert to int if its int
+                                            var i = parseInt(kv[1]);
+                                            if(i == kv[1]) {
+                                                kv[1] = i;
+                                            }
+                                            props[kv[0]] = kv[1];
+                                        });
+                                        jobs[clusterid](props);
+                                        callbacks.forEach(function(callback) {
+                                            callback(props);
+                                        });
+                                    }
+                                } else {
+                                    //console.log("ignoring "+clusterid);
+                                }
+                                */
+                            }
+                        }
+                        fs.close(fd);
+                    });
+                });
+            }
+        });
+    }
+}
+/*
+
+exports.query = function(query, callback) {
+    //submit!
+    condor_query = spawn('condor_q', ['-xml', query]);
+    var stdout = "";
+    var stderr = "";
+    var skipped_header = false;
+    var xml_parser = new xml2js.Parser();
+    var jobs = {};
+    condor_query.stdout.on('data', function (data) {
+        stdout += data;
+        if(skipped_header == false) {
+            var pos = stdout.indexOf("\n<c>\n");
+           if(pos != -1) {
+                stdout = stdout.substring(pos+1);
+                skipped_header = true;
+            }
+        } 
+
+        while(true) {
+            var pos = stdout.indexOf("\n</c>\n");
+            if(pos != -1) {
+                var xml = stdout.substring(0, pos+5);
+                stdout = stdout.substring(pos+6);
+                //consume one xml block
+                //console.log("parsing one block", stdout.length);
+                xml_parser.parseString(xml, function(err, result) {
+                    //console.dir(result); 
+                    var classads = {};
+                    result.c.a.forEach(function(record) {
+                        var name;
+                        var value;
+                        for(var k in record) {
+                            var v = record[k]; 
+                            switch(k) {
+                            case "$": name = v.n; break;
+                            case "i": value = parseInt(v[0]); break;
+                            case "r": value = parseFloat(v[0]); break;
+                            case "e": value = v[0]; break; //TODO ... expression;
+                            case "s": value = v[0]; break;
+                            case "b": value = (v[0].$.v == "t"); break; 
+                            default:
+                                console.log("unknown element type:");
+                                console.dir(record);
+                            }
+                        }
+                        classads[name] = value;
+                    });
+                    //console.log("parsed one classads");
+                    if(jobs[classads.ClusterId] == undefined) {
+                        jobs[classads.ClusterId] = {};
+                    }
+                    jobs[classads.ClusterId][classads.ProcId] = classads;
+                });
+            } else break;
+        }
+    });
+    condor_query.stderr.on('data', function (data) {
+        stderr += data;
+    });
+    condor_query.on('close', function (code) {
+        if(code !== 0) {
+            console.dir(stderr);
+            return undefined;
+        } else {
+            //console.log(jobs.length);
+            //console.dir(jobs);
+            callback(jobs);
+        }
+    });
+}
+
+exports.watch = function(jobs, callback) {
+    for(var clusterid in jobs) {
+        jobs_watching[clusterid] = callback;
+    };
+}
+
+//this is just for experiments
+exports.submit = function(options, success, error) {
+    //create submit file
+    var submit_file = "";
+    submit_file += "universe="+options.universe+"\n";
+    submit_file += "executable="+options.executable+"\n";
+    submit_file += "log=/tmp/log.xml\n";
+    submit_file += "queue\n";
+
+    console.log("submitting following submit file");
+    console.log(submit_file);
+    
+    //submit!
+    condor_submit = spawn('condor_submit', ['-verbose']);
+    condor_submit.stdin.write(submit_file);
+    condor_submit.stdin.end();
+    var stdout = "";
+    condor_submit.stdout.on('data', function (data) {
+        stdout += data;
+    });
+    condor_submit.stderr.on('data', function (data) {
+        console.log('grep stderr: ' + data);
+    });
+    condor_submit.on('close', function (code) {
+        if(code !== 0) {
+            error();
+        } else {
+            //parse stdout
+            var stdout_lines = stdout.split("\n");
+            var job = {id: undefined, classads: {}};
+            stdout_lines.forEach(function(line) {
+                if(line == "") return;
+                if(line.indexOf("** Proc ") == 0) {
+                    job.id = line.substring(9, line.length-1);
+                } else {
+                    parse_classad(line, job.classads);
+                }
+            });
+            success(job);
+        }
+    });
+};
+
+//parse something like
+//JOB_GLIDEIN_ClusterId = "$$(GLIDEIN_ClusterId:Unknown)"
+function parse_classad(line, classads) {
+    var pos = line.indexOf(" = ");
+    var k = line.substring(0, pos);
+    var v = line.substring(pos+3);
+    if(v[0] == '"') {
+        v = v.substring(1, v.length-1);
+    } else {
+        if(v == "true") {
+            v = true;
+        } else if(v == "false") {
+            v = false;
+        } else v = parseFloat(v);
+    }
+    classads[k] = v;
+}
+
+*/
